02322a77b3649c0524568ab98ab55bae3d4de026
David Braley <david.braley@gmail.com>
2019-07-21 08:47:59 -0400

    Extract Node Text

--- link.go
+++ link.go
@@ -1,9 +1,9 @@
 package gophercises_link
 
 import (
-	"fmt"
 	"golang.org/x/net/html"
 	"io"
+	"strings"
 )
 
 type Link struct {
@@ -11,24 +11,54 @@ type Link struct {
 	Text string
 }
 
-var r io.Reader
-
 func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
-	return nil, nil
+	nodes := linkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	msg := n.Data
-	if n.Type == html.ElementNode {
-		msg = "<" + msg + ">"
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
 	}
-	fmt.Println(padding, msg)
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+"  ")
+		ret += text(c) + " "
 	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	var ret []*html.Node
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	} else {
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			ret = append(ret, linkNodes(c)...)
+		}
+
+	}
+	return ret
 }
